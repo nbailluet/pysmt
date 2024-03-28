40ec178285c47849e39c120c5b235d7671a71f8d
Nicolas Bailluet <nicolas.bailluet@inria.fr>
2024-03-28 21:12:31 +0100

    Bump version of Boolector to 3.2.3

--- pysmt/cmd/install.py
+++ pysmt/cmd/install.py
@@ -38,7 +38,7 @@ INSTALLERS = [
     Installer(Z3Installer,      "4.8.17", {"osx": "10.16"}),
     Installer(YicesInstaller,   "2.6.2",
               {"yicespy_version": "f0768ffeec15ea310f830d10878971c9998454ac"}),
-    Installer(BtorInstaller,    "3.2.1", {}),
+    Installer(BtorInstaller,    "3.2.3", {}),
     Installer(PicoSATInstaller, "965",
               {"pypicosat_minor_version" : "1708010052"}),
     Installer(CuddInstaller,    "2.0.3",
